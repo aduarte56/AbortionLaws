f7b760f926cbe9b0b613806297e4482c420a9661
aduarte56 <aduarte56@users.noreply.github.com>
2022-05-27 11:45:48 +0200

    Data Cleaning of the Grounds2022 dataset

--- data_cleaning.R
+++ data_cleaning.R
@@ -13,14 +13,17 @@ library(ggplot2)
 library(rgdal)
 library(readxl)
 library(stringr)
+library(lubridate)
+library(countrycode)
 
 ## ---------------------------
 ## 1.Importing the Maternal Mortality Datasets
 ## ---------------------------
 
-#Maternal Mortality until 2017 no data on age groups
+#Maternal Mortality until 2017 no data on age groups (source: )
 maternal_mortality_17 <-read.csv("Data/MMortalityWHO_2017.csv") 
 
+#
 maternal_mortality_17 <- maternal_mortality_17 %>% clean_names() %>% 
   cbind(str_split_fixed(maternal_mortality_17$Maternal.mortality.ratio..per.100.000.live.births.,"\\[", 2)) %>%
   rename("mortality_ratio"="1", "mortality_treshold"= "2") %>% 
@@ -40,7 +43,7 @@ maternal_mortality_17 <-
   select(country, year, mortality_ratio, number_deaths)
 
 
-#Maternal Mortality until 2021 per age group
+#Maternal Mortality until 2021 (source )
 maternal_mortality_21 <-read.csv("Data/MortalityDatabase2021.csv", sep = ";") 
 maternal_mortality_21 <- maternal_mortality_21 %>% clean_names() %>% 
   select(country_name, year, age_group_code, number, sex,
@@ -51,16 +54,18 @@ maternal_mortality_21 <- maternal_mortality_21 %>% clean_names() %>%
 ## 2.Importing the Global Abortion Policies Datasets
 ## ---------------------------
 
-#Dataset containing types of abortion regulation (Source: UN: https://abortion-policies.srhr.org/)
+#1.Dataset containing types of abortion laws (Source: UN: https://abortion-policies.srhr.org/)
 laws <- read_excel("Data/laws.xlsx", na = "na")
 
-#Dataset containing Abortion incidence in the world (Source:https://osf.io/6t4eh/)
+#2. Dataset containing Abortion incidence in the world (Source:https://osf.io/6t4eh/)
 incidence <- read.csv("Data/AbortionIncidence.csv")
 
-#Dataset containing types of abortion regulation gathered in 2013, but 
-#from 2011 (Source: UN: https://abortion-policies.srhr.org/)
+#3. Dataset containing types of abortion regulation per country gathered in 2013, but 
+#from 2011. Referenced in the original paper.
+#(Source: UN: Economics and social affairs: https://www.un.org/en/development/desa/population/publications/policy/world-abortion-policies-2013.asp)
 grounds2011 <- read_excel("Data/AbortionGrounds2013.xlsx", na = "na")
 
+#cleaning the grounds2011 dataset
 grounds2011 <- grounds2011 %>%  clean_names() %>% na_if("..") %>% 
   rename("save_life"=to_save_a_womans_life_2011_1, "physical_health"=to_preserve_a_womans_physical_health_2011_2, 
          "mental_health"=to_preserve_a_womans_mental_health_2011_3, "rape_incest"=in_case_of_rape_or_incest_2011_4,
@@ -70,13 +75,13 @@ grounds2011 <- grounds2011 %>%  clean_names() %>% na_if("..") %>%
 grounds2011[grounds2011 == "X"] <- "1"
 grounds2011[grounds2011 == "-"] <- "0"
 
+#Creating a column with the aggregate value for the admitted grounds.
 grounds2011 <- grounds2011 %>% mutate("sum_grounds"=as.numeric(save_life)+as.numeric(physical_health)+
                                         as.numeric(mental_health)+as.numeric(rape_incest)+as.numeric(foetal_imp)+
                                         as.numeric(socioec)+as.numeric(on_request))  
 
-#Dataset containing world abortion policies as of 2021 
+#4. Dataset containing world abortion policies as of 2021 per regions in federal states.
 # (Source: UN: https://abortion-policies.srhr.org/)
-
 grounds2022 <- read_excel("Data/Grounds2022.xlsx", na = " ") %>% clean_names()
 
 grounds2022 <- grounds2022 %>%    
@@ -89,16 +94,22 @@ grounds2022 <- grounds2022 %>%
          "rape"=x1g_in_cases_of_rape_yes_no_law_varies_by_jurisdiction,
          "foetal_imp"=x1h_in_cases_of_foetal_impairment_yes_no_law_varies_by_jurisdiction,
          "socioec"=x1i_for_economic_or_social_reasons_yes_no_law_varies_by_jurisdiction, 
-         "other"=x1k_other_please_specify) 
-
-
+         "other"=x1k_other_please_specify,
+         "national_jurisdiction"=country) %>% 
+  mutate(country=countrycode(iso_code, origin = 'iso2c', destination = 'country.name'))
 
 grounds2022[grounds2022 == "Yes"] <- "1"
 grounds2022[grounds2022 == "No"] <- "0"
-grounds2022[grounds2022 == "Law Varies By Jurisdiction"] <- "0.5"
-grounds2022[grounds2022 == is.na(grounds2022)] <- 0
+grounds2022[grounds2022 == "Law Varies By Jurisdiction"] <-"0.5"
+grounds2022[6:14] <- lapply(grounds2022[6:14], as.numeric)
 
-grounds2022 <- grounds2022 %>% mutate("sum_grounds2022"=as.numeric(save_life)+ as.numeric(health)+as.numeric(physical_health)+
-                                        as.numeric(mental_health)+as.numeric(cognitive_disability)+as.numeric(rape)+as.numeric(incest)+
-                                        as.numeric(foetal_imp)+
-                                        as.numeric(socioec))  
+#Adding the grounds into a flexibility aggregated variable
+grounds2022 <- grounds2022 %>% mutate("sum_grounds2022"=rowSums(grounds2022[6:14], na.rm=TRUE))
+
+#Setting a column for year, date and month of the latest abortion policy per country.
+grounds2022 <- grounds2022 %>% mutate(date_completed=dmy(date_completed)) %>% 
+  mutate(year_completed=year(date_completed), month_completed=month(date_completed), day_completed=day(date_completed)) %>% 
+  relocate(country, .after = national_jurisdiction) %>% 
+  relocate(c(year_completed, month_completed,day_completed), .after = date_completed)
+
+
