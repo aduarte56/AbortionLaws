c5eb2dfb3aa86adc5a76185acd4005ee5175659c
aneleh-cikab <90176475+aneleh-cikab@users.noreply.github.com>
2022-06-08 13:40:38 +0200

    Heterogeinity analysis and plots

--- 1-causal_models.R
+++ 1-causal_models.R
@@ -38,8 +38,6 @@ summary(model)
 model1 <- lm(data=df1, mortality_ratio_17~flex_binary_score+gdp+country_name+period)
 summary(model1)
 
-#Linear regression model: mortality rate on binary flexibility score controlling for 
-#gdp, primary completion, countries and periods
 model2 <- lm(data=df, mortality_ratio_17~flex_binary_score+gdp+primary_completion+country_name+period)
 summary(model2)
 
@@ -53,6 +51,7 @@ summary(model2)
 #______________________________________________________________________________
 
 
+##
 ##Calculating ATE using G-separation
 ## Create predicted Y_A for all observations, sets flex_score=f
 Ypred<-predict(model1)
@@ -66,21 +65,30 @@ Y1<-predict(model1,newdata=df.flex_score1)
 ## Calculate (Y_1 - Y_0) for each individual
 ATE<-Y1-Y0
 
-##Including the actual observations for flexibility scores in the dataset
-## to filter treatment settings and calculate ATT and ATU
+##Calculating ATT using G-separation
+
 df.flex_score0 <- df.flex_score0 %>% mutate(observed_bin_score=df1$flex_binary_score)
 df.flex_score1<- df.flex_score1 %>% mutate(observed_bin_score=df1$flex_binary_score)
 
-#calculating ATT:
-df_T0 <- df.flex_score0 %>% filter(observed_bin_score==1) #imputed treatment 0 among those who were treated
+df_T0 <- df.flex_score0 %>% filter(observed_bin_score==1) #imputed results for treatment 0 for those who were treated
+#df_T<-df1 %>% filter(flex_binary_score==1)
 Y_T0<-predict(model1,newdata=df_T0) # prediction for imputed treatment 0
-ATT<-mean(df_T0$mortality_ratio_17-Y_T0) # difference between prediction and results
-
+#Y_T1<-predict(model1,newdata=df_T)
+ATT<-mean(Y_T0-df_T0$mortality_ratio_17) # difference between prediction and results
 ##Calculating ATU using G-separation
-df_T1 <- df.flex_score0 %>% filter(observed_bin_score==0) #imputed treatment 1 for those who were untreated
-Y_T1<-predict(model1,newdata=df_T1) # prediction for imputed treatment 1
-ATU<-mean(Y_T1-df_T1$mortality_ratio_17)
 
+df_U <- df1 %>% filter(flex_binary_score==0)
+df_U1<-transform(df_U, flex_binary_score=1) 
+Y_U0.S1<-predict(model1,newdata=df_U) 
+Y_U.S1<-predict(model1,newdata=df_U1)
+ATU<-Y_U0.S1-Y_U.S1
+
+
+df_T1 <- df.flex_score0 %>% filter(observed_bin_score==0) #imputed results for treatment 0 for those who were treated
+#df_T<-df1 %>% filter(flex_binary_score==1)
+Y_T1<-predict(model1,newdata=df_T1) # prediction for imputed treatment 0
+#Y_T1<-predict(model1,newdata=df_T)
+ATU<-mean(Y_T1-df_T1$mortality_ratio_17)
 #------------------------------------------------------------------------------
 # Sensitivity Analysis
 #______________________________________________________________________________
@@ -101,10 +109,8 @@ df.sensitivity <- sensemakr(model = model2,
 summary(df.sensitivity)
 
 plot(df.sensitivity)
-
-
 #------------------------------------------------------------------------------
-# Heterogeneity of effects
+# Heterogeneity of effects (lab4)
 #______________________________________________________________________________
 
 library(tidyverse)
@@ -140,7 +146,7 @@ resid_full == r
 
 data <- mutate(data, w = resid_treatment^2)
 
-#Now, who gets the highest weights? We'll average the weights for observations
+#Who gets the highest weights? We'll average the weights for observations
 #from each country in all of its observations (in different years)
 
 country <- summarise(group_by(data, country_name), mean_w = mean(w))
@@ -148,18 +154,36 @@ country <- summarise(group_by(data, country_name), mean_w = mean(w))
 #And we can look at the *least* influential countries in our ATE:
 
 
-least_influential <- arrange(country, mean_w)
-head(least_influential) #least influential countries
-tail(least_influential) #most influential countries
+influence <- arrange(country, mean_w)
+head(influence) #least influential countries
+tail(influence) #most influential countries
 
 
-ggplot(filter(data, country_name == "Mexico"), 
-       aes(flexibility_score, mortality_ratio_17)) + 
-  geom_point() 
+  
+least <- c("South Sudan", "Venezuela", "Luxembourg", "Finland", "Brazil", "Israel")
+most <- c("SÃ£o TomÃ© & PrÃ­ncipe", "Angola", "Ireland", "Nepal", "Colombia", "Mexico")
+  
+least_influential <- data %>%
+    filter(country_name %in% least) %>%
+    ggplot(aes(flexibility_score, mortality_ratio_17)) +
+    geom_point(position = "jitter") +
+    labs(x = "Flexibility score",
+         y = "MMR") +
+    theme_bw() +
+    facet_wrap(vars(country_name))
 
-ggplot(filter(data, country_name == "Venezuela"), 
-       aes(flexibility_score, mortality_ratio_17)) + 
-  geom_point() 
+ggsave("Figures/least.png", dpi = 200, least_influential)
+
+most_influential <- data %>%
+  filter(country_name %in% most) %>%
+  ggplot(aes(flexibility_score, mortality_ratio_17)) +
+  geom_point(position = "jitter") +
+  labs(x = "Flexibility score",
+       y = "MMR") +
+  theme_bw() +
+  facet_wrap(vars(country_name))
+
+ggsave("Figures/most.png", dpi = 200, most_influential)
 
 #effective sample
 
@@ -168,9 +192,54 @@ effective_sample <- data %>% group_by(country_name) %>%
   mutate(weight_avg = weight/sum(weight),
          weight_pct = weight_avg * 100)
 
-effective_sample %>% arrange(weight_pct, decreasing = TRUE) %>%
+effective_sample %>% arrange(desc(weight_pct)) %>%
   mutate(cum_sum = cumsum(weight_pct)) %>%
-  head()
+  filter(cum_sum <= 52)
+
+#plotting nominal and effective sample
+
+nominal_sample <- effective_sample %>% arrange(desc(weight_pct)) %>%
+  mutate(cum_sum = cumsum(weight_pct)) %>%
+  filter(cum_sum <= 52) %>%
+  pull(country_name)
+
+nominal_sample[nominal_sample=="SÃ£o TomÃ© & PrÃ­ncipe"] <- "Sao Tome and Principe"
+nominal_sample[nominal_sample=="CÃ´te dâ€™Ivoire"] <- "Côte d'Ivoire"
+nominal_sample[nominal_sample=="Eswatini"] <- "Swaziland"
+nominal_sample %in% world$brk_name
+
+
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+nominal_world <- world %>% filter(brk_name %in% nominal_sample)
+
+#effective sample plot
+effective_plot <- ggplot(data = world) +
+  geom_sf(data = world, fill = "white") +
+  geom_sf(data = nominal_world, fill = "red")
+
+ggsave("Figures/effective_sample.png", dpi = 200, effective_plot)
+
+#full sample plot
+full_sample <- data %>% 
+  distinct(country_code) %>%
+  pull(country_code)
+
+full_sample %in% world$iso_a3 
+
+full_world <- world %>% filter(iso_a3 %in% full_sample)
+
+full_plot <- ggplot(data = world) +
+  geom_sf(data = world, fill = "white") +
+  geom_sf(data = full_world, fill = "black")
+
+ggsave("Figures/full_sample.png", dpi = 200, full_plot)
+
+
+
+  
 
 
 - Test positivity assumption. 
